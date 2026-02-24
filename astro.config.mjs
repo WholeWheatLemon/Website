4f0a3b8965a3b32a3a53220a7bae118e16849636
ollie <me@ollie.lol>
2026-02-24 18:32:36 -0500

    new site attempt 2

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-    site: "https://ollie.lol",
-    trailingSlash: "ignore",
-    devToolbar: {
-        enabled: false
-    }
-})
